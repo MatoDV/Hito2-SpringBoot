c5f04fd26d6d0969f298366084c0255860788e21
MatoDV <matias.brizuela@davinci.edu.ar>
2024-06-25 20:04:55 -0300

    feat: adding the folder with the spring boot initializr

---
+++ hito2/src/main/java/com/ejercicio/hito2/Hito2Application.java
@@ -0,0 +1,13 @@
+package com.ejercicio.hito2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hito2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hito2Application.class, args);
+	}
+
+}
---
+++ hito2/src/test/java/com/ejercicio/hito2/Hito2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejercicio.hito2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hito2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
