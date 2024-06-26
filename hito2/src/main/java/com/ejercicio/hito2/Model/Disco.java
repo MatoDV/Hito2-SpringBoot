f748d4d52d1dbca0541d7058ce93913e0fcecedd
Matias Brizuela <matias.brizuela@davinci.edu.ar>
2024-06-26 12:54:35 -0300

    feat: complete class Disco and create ArtistaRepository

--- hito2/src/main/java/com/ejercicio/hito2/Model/Disco.java
+++ hito2/src/main/java/com/ejercicio/hito2/Model/Disco.java
@@ -15,5 +15,59 @@ public class Disco {
 
     @ElementCollection
     private List<String> generos;
-    
+
+    @ElementCollection
+    private List<String> canciones;
+
+    @ManyToOne
+    @JoinColumn(name = "artista_id")
+    private Artista artista;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaDeNacimiento() {
+        return fechaDeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(LocalDate fechaDeNacimiento) {
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+
+    public List<String> getGeneros() {
+        return generos;
+    }
+
+    public void setGeneros(List<String> generos) {
+        this.generos = generos;
+    }
+
+    public List<String> getCanciones() {
+        return canciones;
+    }
+
+    public void setCanciones(List<String> canciones) {
+        this.canciones = canciones;
+    }
+
+    public Artista getArtista() {
+        return artista;
+    }
+
+    public void setArtista(Artista artista) {
+        this.artista = artista;
+    }
 }
---
+++ hito2/src/main/java/com/ejercicio/hito2/Repository/ArtistaRepository.java
@@ -0,0 +1,4 @@
+package com.ejercicio.hito2.Repository;
+
+public interface ArtistaRepository {
+}
